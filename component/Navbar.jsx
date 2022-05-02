810b6f169cf48bfd045fce1da17cfe1cfec1ab16
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-05-02 22:49:40 +0600

    Add files via upload

---
+++ component/Navbar.jsx
@@ -0,0 +1,45 @@
+import styles from '../styles/Navbar.module.css'
+import Image from 'next/image'
+import { useSelector } from 'react-redux'
+import Link from 'next/link'
+
+const Navbar = () => {
+
+  const quantity = useSelector((state) => state.cart.quantity)
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src='/img/telephone.png' alt="" height="40" width="40" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>01234786</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+        <Link href={'/'} passHref>
+          <li className={styles.listItem}>HomePage</li>
+        </Link>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src='/img/logo.png' alt='' height={90} width={110} />
+          <li className={styles.listItem}>Evets</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <Link href={'/cart'} passHref className={styles.cartClick}>
+        <div className={styles.item}>
+          <div className={styles.cart}>
+            <Image src='/img/cart.png' alt='' height={30} width={30} />
+            <div className={styles.counter}>{quantity}</div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ component/PizzaList.jsx
@@ -0,0 +1,18 @@
+import styles from '../styles/PizzaList.module.css'
+import PizzaCard from './PizzaCard'
+
+const PizzaList = ({pizzaList}) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN YOUR AREA</h1>
+      <p className={styles.desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea facilis voluptate explicabo! Molestias eius mollitia dicta magni assumenda voluptas! Maiores nam blanditiis non!</p>
+      <div className={styles.wrapper}>
+      {pizzaList.map((pizza) => (
+        <PizzaCard key={pizza._id} pizza={pizza} />
+      ))}
+      </div>
+    </div>
+  )
+}
+
+export default PizzaList
