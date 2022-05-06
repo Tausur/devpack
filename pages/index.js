6bec88ec8e5879825a7286ac1f3a10b6204910fe
Tausur Rahaman <92077218+Tausur@users.noreply.github.com>
2022-05-06 14:44:51 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Featured from '../component/Featured'
 import PizzaList from '../component/PizzaList'
 import axios from 'axios'
